e21241c7d5c1badedb805ed612af28a3f9a58cde
Philippe Voinov <philippevoinov@gmail.com>
2019-10-11 22:58:45 +0200

    Move app to components folder

--- unilog-client/src/index.tsx
+++ unilog-client/src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { App } from "./app";
+import { App } from "./components/app";
 
 ReactDOM.render(<App />, document.getElementById("root"));
