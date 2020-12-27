75148ac485e32fc90b1824ca269f688d5384f588
Moritz Schneider <moritsch@student.ethz.ch>
2020-12-27 17:29:29 +0100

    add world type

--- unilog-shared/src/interfaces/data.ts
+++ unilog-shared/src/interfaces/data.ts
@@ -1,14 +1,72 @@
 export interface State {
-  buckets: Bucket[];
+  world: MapWorld;
 }
 
-export interface Bucket {
-  id: string;
+export interface MapWorld {
+  compressionlevel: number;
+  editorsettings: EditorSettings;
+  height: number;
+  infinite: boolean;
+  layers: Layer[];
+  nextlayerid: number;
+  nextobjectid: number;
+  orientation: string;
+  renderorder: string;
+  tiledversion: string;
+  tileheight: number;
+  tilesets: Tileset[];
+  tilewidth: number;
+  type: string;
+  version: number;
+  width: number;
+}
+
+export interface EditorSettings {
+  export: Export;
+}
+
+export interface Export {
+  target: string;
+}
+
+export interface Layer {
+  data?: number[];
+  height?: number;
+  id: number;
   name: string;
-  balls: Ball[];
+  opacity: number;
+  properties?: Property[];
+  type: string;
+  visible: boolean;
+  width?: number;
+  x: number;
+  y: number;
+  draworder?: string;
+  objects?: any[];
 }
 
-export interface Ball {
-  id: string;
-  color: string;
+export interface Tileset {
+  columns: number;
+  firstgid: number;
+  image: string;
+  imageheight: number;
+  imagewidth: number;
+  margin: number;
+  name: string;
+  spacing: number;
+  tilecount: number;
+  tileheight: number;
+  tilewidth: number;
+  tiles?: Tile[];
+}
+
+export interface Tile {
+  id: number;
+  properties: Property[];
+}
+
+export interface Property {
+  name: string;
+  type: string;
+  value: any;
 }
