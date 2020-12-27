e72163e857aa250ab8ec2749a6ebfb4fd4434ef3
Moritz Schneider <moritsch@student.ethz.ch>
2020-12-27 18:53:05 +0100

    fix server

--- unilog-server/src/fake.ts
+++ unilog-server/src/fake.ts
@@ -1,28 +1,3 @@
 import { Action, ActionType } from "unilog-shared";
 
-export const FAKE_ACTIONS: Action[] = [
-  {
-    type: ActionType.CreateBall,
-    id: "1",
-    color: "red",
-    bucketId: "philippes-bucket",
-  },
-  {
-    type: ActionType.CreateBall,
-    id: "2",
-    color: "green",
-    bucketId: "philippes-bucket",
-  },
-  {
-    type: ActionType.CreateBall,
-    id: "4",
-    color: "green",
-    bucketId: "other-bucket",
-  },
-  {
-    type: ActionType.CreateBall,
-    id: "6",
-    color: "blue",
-    bucketId: "other-bucket",
-  },
-];
+export const FAKE_ACTIONS: Action[] = [];
