2824d35d40b8d192d9a19b5e996bcc59443e8de3
Moritz Schneider <moritsch@student.ethz.ch>
2020-12-28 14:53:37 +0100

    .

--- unilog-client/src/components/map-display.tsx
+++ unilog-client/src/components/map-display.tsx
@@ -2,16 +2,16 @@ import React, { useEffect, useMemo, useRef } from "react";
 import { DisplayTile } from "../interfaces";
 import { getDisplayTilesFunction } from "../get-display-tiles";
 import { Coordinates } from "unilog-shared";
+import * as glTiled from "gl-tiled";
 
 interface Props {
-  getDisplayTiles: getDisplayTilesFunction;
   onPointerDown: (coordinates: Coordinates, ev: React.PointerEvent) => void;
   onPointerMove: (coordinates: Coordinates, ev: React.PointerEvent) => void;
   onPointerUp: (coordinates: Coordinates, ev: React.PointerEvent) => void;
 
   width: number;
   height: number;
-  pixelScale: number; // number of physical pixels per sprite pixel
+  tilemap: glTiled.GLTilemap;
   offset: Coordinates; // number of tiles to shift by before drawing. When zero, the (0, 0) tile will draw in the top left corner of the canvas (possibly fractional)
   tileSize: number; // width (and height) of a tile in pixels of the source image
 }
@@ -22,55 +22,17 @@ const styles = {
   } as React.CSSProperties,
 };
 
-// extractTileImage draws a single tile from a sprite sheet into the supplied canvas context.
-// ctx must have width == height == tileSize
-function extractTileImage(tile: DisplayTile, ctx: CanvasRenderingContext2D) {
-  ctx.save();
-
-  const tileSize = tile.rectangle.width;
-  if (tile.rectangle.height !== tileSize) {
-    throw new Error("tiles must be square");
-  }
-
-  ctx.translate(tileSize / 2, tileSize / 2);
-  if (tile.flips.diagonal) {
-    ctx.rotate(-Math.PI / 2);
-    ctx.scale(1, -1);
-  }
-  if (tile.flips.horizontal) {
-    ctx.scale(-1, 1);
-  }
-  if (tile.flips.vertical) {
-    ctx.scale(1, -1);
-  }
-  ctx.translate(-tileSize / 2, -tileSize / 2);
-
-  ctx.drawImage(
-    tile.image,
-    tile.rectangle.x,
-    tile.rectangle.y,
-    tileSize,
-    tileSize,
-    0,
-    0,
-    tileSize,
-    tileSize,
-  );
-
-  ctx.restore();
-}
-
 export const MapDisplay: React.FC<Props> = ({
-  getDisplayTiles,
-  width,
-  height,
-  pixelScale,
+  tilemap,
   offset,
   tileSize,
   onPointerDown,
   onPointerMove,
   onPointerUp,
 }) => {
+  const width = tilemap.viewportWidth;
+  const height = tilemap.viewportHeight;
+
   const canvas: React.Ref<HTMLCanvasElement> = useRef(null);
   const canvasWidth = Math.floor((width / pixelScale) * devicePixelRatio);
   const canvasHeight = Math.floor((height / pixelScale) * devicePixelRatio);
@@ -87,55 +49,29 @@ export const MapDisplay: React.FC<Props> = ({
   }, [tileSize]);
 
   const render = () => {
-    const ctx = canvas.current?.getContext("2d");
-    if (!ctx) {
+    const gl = canvas.current?.getContext("webgl");
+    if (!gl) {
       return;
     }
 
-    // TODO not necessary to clear background, this is only for debugging
-    ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, width, height);
-
-    const firstTileCoords = {
-      x: Math.floor(offset.x / tileSize),
-      y: Math.floor(offset.y / tileSize),
-    };
-    const lastTileCoords = {
-      x: Math.ceil(offset.x + canvasWidth / tileSize),
-      y: Math.ceil(offset.y + canvasHeight / tileSize),
-    };
-
-    for (
-      let tileCoords = { ...firstTileCoords };
-      tileCoords.y <= lastTileCoords.y;
-      tileCoords =
-        tileCoords.x <= lastTileCoords.x
-          ? {
-              x: tileCoords.x + 1,
-              y: tileCoords.y,
-            }
-          : { x: firstTileCoords.x, y: tileCoords.y + 1 }
-    ) {
-      const tileCornerDest = {
-        x: Math.floor((tileCoords.x - offset.x) * tileSize),
-        y: Math.floor((tileCoords.y - offset.y) * tileSize),
-      };
-
-      const displayTiles = getDisplayTiles(tileCoords);
-      for (const displayTile of displayTiles) {
-        extractTileImage(displayTile, tempTileCtx);
-
-        const { image, rectangle } = displayTile;
-        ctx.drawImage(
-          tempTileCtx.canvas,
-          tileCornerDest.x,
-          tileCornerDest.y,
-          rectangle.width,
-          rectangle.height,
-        );
-      }
+    // HACK shader compilation crashes if there are not tilesets
+    if (!tilemap.tilesets.filter((tileset) => tileset.images.length).length) {
+      return;
     }
+
+    if (tilemap.gl !== gl) {
+      console.log("DEBUG tilemap.glInitialize");
+      tilemap.glInitialize(gl);
+    }
+
+    tilemap.draw();
   };
+
+  useEffect(() => {
+    return () => {
+      tilemap.glTerminate();
+    };
+  }, [tilemap]);
 
   useEffect(() => {
     const frameRequestHandle = requestAnimationFrame(() => {
